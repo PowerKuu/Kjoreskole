e6c7293f7be66e4334bb16a1ab735a6a9f2502fd
PowerKuu <hkleven6@outlook.com>
2023-03-17 15:26:59 +0100

    update

---
+++ src/components/CarGame.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted } from "vue"
+
+var canvas:HTMLCanvasElement
+
+onMounted(() => {
+    var ctx = canvas.getContext('2d')
+    const parent = canvas.parentElement
+    if (!ctx || !parent) return
+
+    window.onresize = () => {
+        canvas.width = parent.offsetWidth
+        canvas.height = parent.offsetHeight
+    }
+})
+
+
+</script>
+
+<template>
+<canvas class="canvas" ref="canvas"></canvas>
+</template>
+
+<style scoped>
+</style>
