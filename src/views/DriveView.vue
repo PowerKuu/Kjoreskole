8bfff709fde1ef9b2ddffa1e973e6900ef76d184
PowerKuu <hkleven6@outlook.com>
2023-03-30 13:25:22 +0200

    update

---
+++ src/views/DriveView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Flex from "@/components/FlexSystem.vue"
+import Button from "@/components/Button.vue"
+import DriverCard from "@/components/DriverCard.vue"
+</script>
+<template>
+    <div class="">
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
